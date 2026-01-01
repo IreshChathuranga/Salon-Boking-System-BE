8e52e03066672014821b7bc7b65e0f9726bd7a31
IreshChathuranga <ireshchathuranga63309.com>
2026-01-01 22:08:12 +0530

    change origin URL

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://salon-boking-system-fe-uz6m.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
