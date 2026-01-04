9a6ec52ce2ceb3a658f17148fc1ea088c918ce9c
IreshChathuranga <ireshchathuranga63309.com>
2026-01-04 12:01:45 +0530

    add new baseURL

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ const app = express()
 app.use(
   cors({
     origin: ["http://localhost:5173","https://salon-boking-system-fe.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 )
